45a4b2e8637e217a27b12ccf9160525b2559dc54
Yen Ong <yen.ong@amd.com>
2025-11-26 19:29:55 +0000

    ranking test new API

--- shared/origami/python/origami_rank_test.py
+++ shared/origami/python/origami_rank_test.py
@@ -32,7 +32,24 @@ def build_tile_list(library_name):
     for item in solutions:
         tilelist.append(tuple([item[col] for col in KERNEL_COLUMNS]))
     return tilelist
-        
+
+def read_configs_list(library_name):
+    configs_list = []
+    with open("./tiles_library/" + library_name, "rt") as libinput:
+        solutions = yaml.load(libinput, Loader=yaml.SafeLoader)
+    for item in solutions:
+        config = origami.config_t()
+        config.mt = origami.dim3_t(item['MT_M'], item['MT_N'], item['MT_K'])
+        config.mi = origami.dim3_t(item['MI_M'], item['MI_N'], item['MI_K'])
+        config.occupancy = max(item['Occupancy'], 1)
+        config.workgroup_mapping = item['WGM']
+        config.cache_hints_a = item['NTA']
+        config.cache_hints_b = item['NTB']
+        config.workspace_size = 2**64-1
+        config.workspace_size_per_elem_c = 2**64-1
+        configs_list.append(config)
+    return configs_list
+
 def main():
     args = parseArguments()
     
@@ -40,7 +57,6 @@ def main():
     if not(Path(benchyaml).is_file()):
         print(f"Yaml file {args.yaml} not found.")
         exit(1)
-    resultsfile = Path(benchyaml).stem + ".out"
 
     hardware = origami.get_hardware_for_device(args.device)
 
@@ -51,15 +67,21 @@ def main():
         benchmarks = yaml.load(finput, Loader=yaml.SafeLoader)
     
     print(f"Loaded yaml with {len(benchmarks)} size(s).")
+    resultsfile = Path(benchyaml).stem + ".out"
+
+    # Load Baseline
+    with open("./baseline/results/" + resultsfile, "rt") as finput:
+        base_rankings = yaml.load(finput, Loader=yaml.SafeLoader)
 
     prefix = 'gfx950_Cijk_'
     transA_coord = {'N': 'Ailk', 'T': 'Alik'}
     transB_coord = {'N': 'Bljk', 'T': 'Bjlk'}
     datadict = {"bf16": "BBS", "f32": "SB", "xf32": "S_MX"}
     hblt_to_origami = {"bf16_r": "bf16", "f32_r": "f32"}
-
+    
+    lib_cache = {}
     sizes_list = []
-    for bench_item in benchmarks:
+    for idx, bench_item in enumerate(benchmarks):
         solutionlib = prefix + transA_coord[bench_item['transA']]
         solutionlib += "_" + transB_coord[bench_item['transB']]
     
@@ -70,41 +92,72 @@ def main():
         else:
             solutionlib += "_" + datadict['f32']
         solutionlib += "_tiles.yaml"
-    
-        tile_list = build_tile_list(solutionlib)
-        transA = bench_item['transA']=='T'
-        transB = bench_item['transB']=='T'
-        type_a = hblt_to_origami[bench_item['a_type']]
-        type_b = hblt_to_origami[bench_item['b_type']]
-        type_d = hblt_to_origami[bench_item['d_type']]
-        result_tuple = origami.select_best_macro_tile_size(
-                        bench_item['M'],
-                        bench_item['N'],
-                        bench_item['K'],
-                        bench_item['batch_count'],
-                        transA,
-                        transB,
-                        hardware,
-                        tile_list,
-                        origami.datatype_to_bits(origami.string_to_datatype(type_a)),
-                        origami.datatype_to_bits(origami.string_to_datatype(type_b)),
-                        origami.datatype_to_bits(origami.string_to_datatype(type_d)),
-                        origami.string_to_datatype(type_a),
-                        0,
-                        0.8,
-                        args.print,
-                        6,
-                    )
-        results_list = []
-        for rank_item in result_tuple:
-            if rank_item[0] < 1e12:
-                rv = {key:round(value) for key, value in zip(['Latency'] + KERNEL_COLUMNS, rank_item)}
-                results_list.append(rv)
-        sizes_list.append(results_list)
+        
+        if solutionlib in lib_cache:
+            configs_list = lib_cache[solutionlib]
+        else:
+            configs_list = read_configs_list(solutionlib)
+            lib_cache[solutionlib] = configs_list
+        
+        problem = origami.problem_t()
+        problem.size = origami.dim3_t(bench_item['M'], bench_item['N'], bench_item['K'])
+        problem.batch = bench_item['batch_count']
+        problem.a_transpose = origami.transpose_t.T if bench_item['transA']=='T' else origami.transpose_t.N
+        problem.b_transpose = origami.transpose_t.T if bench_item['transB']=='T' else origami.transpose_t.N
+        problem.a_dtype = origami.string_to_datatype(hblt_to_origami[bench_item['a_type']])
+        problem.b_dtype = origami.string_to_datatype(hblt_to_origami[bench_item['b_type']]) 
+        problem.d_dtype = origami.string_to_datatype(hblt_to_origami[bench_item['d_type']])
+        problem.c_dtype = problem.d_dtype
+        problem.mi_dtype = problem.a_dtype
+        problem.a_mx_block_size = 0
+        problem.b_mx_block_size = 0
 
-    yaml_args = {"default_flow_style": None, "sort_keys": False}
-    with open("./bench_sizes/results/" + resultsfile, "wt") as foutput:
-        yaml.dump(sizes_list, foutput, **yaml_args)
+        rv = origami.rank_configs(problem, hardware, configs_list)
+        # results_list = []
+        # for rank_item in rv:
+        #     if rank_item.latency < 1e12:
+        #         results_list.append({'Latency': round(rank_item.latency), \
+        #                              'MT_M': rank_item.config.mt.m, \
+        #                              'MT_N': rank_item.config.mt.n, \
+        #                              'MT_K': rank_item.config.mt.k, \
+        #                              'MI_M': rank_item.config.mi.m, \
+        #                              'MI_N': rank_item.config.mi.n, \
+        #                              'MI_K': rank_item.config.mi.k, \
+        #                              'Occupancy': rank_item.config.occupancy, \
+        #                              'WGM': rank_item.config.workgroup_mapping, \
+        #                              'NTA': rank_item.config.cache_hints_a, \
+        #                              'NTB': rank_item.config.cache_hints_b})
+        # sizes_list.append(results_list)
+
+        count = len(base_rankings[idx])
+        print(f"----------size {bench_item['M']}, {bench_item['N']}, {bench_item['K']}")
+        for kk, (base_item, new_item) in enumerate(zip(base_rankings[idx], rv[:count])):
+            try:
+                assert base_item['Latency']==round(new_item.latency) and \
+                    base_item['MT_M']==new_item.config.mt.m and \
+                    base_item['MT_N']==new_item.config.mt.n and \
+                    base_item['MT_K']==new_item.config.mt.k and \
+                    base_item['MI_M']==new_item.config.mi.m and \
+                    base_item['MI_N']==new_item.config.mi.n and \
+                    base_item['MI_K']==new_item.config.mi.k, "Solution ranking not identical"
+            except AssertionError as e:
+                    print('Latency', base_item['Latency'], round(new_item.latency))
+                    print('MT_M', base_item['MT_M'], new_item.config.mt.m)
+                    print('MT_N', base_item['MT_N'], new_item.config.mt.n)
+                    print('MT_K', base_item['MT_K'], new_item.config.mt.k)
+                    print('MI_M', base_item['MI_M'], new_item.config.mi.m)
+                    print('MI_N', base_item['MI_N'], new_item.config.mi.n)
+                    print('MI_K', base_item['MI_K'], new_item.config.mi.k)
+                    break
+                #    base_item['Occupancy']==new_item.config.occupancy and \
+                #    base_item['WGM']==new_item.config.workgroup_mapping and \
+                #    base_item['NTA']==new_item.config.cache_hints_a and \
+                #    base_item['NTB']==new_item.config.cache_hints_b, "Solution ranking not identical"
+
+        print(f"Benchmark {idx} matches")
+    # yaml_args = {"default_flow_style": None, "sort_keys": False}
+    # with open("./bench_sizes/results/" + resultsfile, "wt") as foutput:
+    #     yaml.dump(sizes_list, foutput, **yaml_args)
 
 if __name__ == "__main__":
     exit(main())
