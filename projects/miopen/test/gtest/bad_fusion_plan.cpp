69526d836a2f94b5a057b5fb65f4112cac23b27b
Jonathan Lichtner <195780826+JonathanLichtnerAMD@users.noreply.github.com>
2026-01-05 23:37:24 +0000

    wip

--- projects/miopen/test/gtest/bad_fusion_plan.cpp
+++ projects/miopen/test/gtest/bad_fusion_plan.cpp
@@ -159,6 +159,9 @@ using namespace bad_fusion_plan;
 
 TEST(GPU_FusionPlan_FP16, GoodFusionPlan)
 {
+#if !MIOPEN_USE_COMPOSABLEKERNEL
+    GTEST_SKIP() << "Test requires CK";
+#endif
     GPU_FusionPlan_FP16<miopen::solver::fusion::ConvCKIgemmFwdBiasActivFused, half_float::half> obj(
         miopenTensorNHWC, miopenActivationRELU);
     if(obj.Skip())
@@ -171,6 +174,9 @@ TEST(GPU_FusionPlan_FP16, GoodFusionPlan)
 
 TEST(GPU_FusionPlan_FP16, BadOrderFusionPlan)
 {
+#if !MIOPEN_USE_COMPOSABLEKERNEL
+    GTEST_SKIP() << "Test requires CK";
+#endif
     GPU_FusionPlan_FP16<miopen::solver::fusion::ConvCKIgemmFwdBiasActivFused, half_float::half> obj(
         miopenTensorNHWC, miopenActivationRELU);
     if(obj.Skip())
