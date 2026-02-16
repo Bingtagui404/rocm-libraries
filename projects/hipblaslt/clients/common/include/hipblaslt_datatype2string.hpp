9224009c1176718ee8391fa24efd0a27ceeaa8ff
Tony Davis <tony.davis@amd.com>
2026-02-16 19:47:30 +0000

    Add special-FP test: +0, -0, +Inf, -Inf, NaN inits and matmul_special_fp_64x64

--- projects/hipblaslt/clients/common/include/hipblaslt_datatype2string.hpp
+++ projects/hipblaslt/clients/common/include/hipblaslt_datatype2string.hpp
@@ -40,6 +40,10 @@ enum class hipblaslt_initialization
     zero       = 555,
     norm_dist  = 666,
     uniform_01 = 777,
+    inf        = 888,
+    neg_zero   = 889,
+    neg_inf    = 890,
+    nan        = 891,
 };
 
 typedef enum class _hipblaslt_activation_type
@@ -191,6 +195,14 @@ constexpr auto hipblaslt_initialization2string(hipblaslt_initialization init)
         return "norm_dist";
     case hipblaslt_initialization::uniform_01:
         return "uniform_01";
+    case hipblaslt_initialization::inf:
+        return "inf";
+    case hipblaslt_initialization::neg_zero:
+        return "neg_zero";
+    case hipblaslt_initialization::neg_inf:
+        return "neg_inf";
+    case hipblaslt_initialization::nan:
+        return "nan";
     }
     return "invalid";
 }
@@ -212,6 +224,10 @@ inline hipblaslt_initialization string2hipblaslt_initialization(const std::strin
         value == "zero"       ? hipblaslt_initialization::zero       :
         value == "norm_dist"  ? hipblaslt_initialization::norm_dist  :
         value == "uniform_01" ? hipblaslt_initialization::uniform_01 :
+        value == "inf"        ? hipblaslt_initialization::inf        :
+        value == "neg_zero"  ? hipblaslt_initialization::neg_zero   :
+        value == "neg_inf"   ? hipblaslt_initialization::neg_inf   :
+        value == "nan"       ? hipblaslt_initialization::nan       :
         static_cast<hipblaslt_initialization>(0);
 }
 // clang-format on
