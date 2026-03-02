0d56843932285cc8a5c9946d9e8f8b733f148edb
isaacbuckingham <isaacbuckingham5@gmail.com>
2026-03-02 12:07:58 -0500

    added ssh keys

--- shared/rocroller/.jenkins/common.groovy
+++ shared/rocroller/.jenkins/common.groovy
@@ -225,7 +225,9 @@ def runPerformanceCommand (platform, project)
                 set -ex
                 cd ${project.paths.project_build_prefix}/
                 eval `ssh-agent`
-                ssh-add \$ENTERPRISE_KEY_FILE
+                ssh-add ${PUBLIC_KEY_FILE}
+                ssh-add ${ENTERPRISE_KEY_FILE}
+                ssh-add -L
                 [ -d gemmaiperf ] && rm -rf gemmaiperf
                 git clone git@github.com:ROCm/gemmaiperf.git
 
